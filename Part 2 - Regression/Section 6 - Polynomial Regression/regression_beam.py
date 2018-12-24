20eaaeb6dd6fa1dd8b1405e0ef7e26c6a6f56676
Philipp Dominic Siedler <p.d.siedler@gmail.com>
2018-12-24 13:08:24 +0000

    adding R files for polynomial regression

---
+++ Part 2 - Regression/Section 6 - Polynomial Regression/regression_beam.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+points = np.array([(3000, 500), (4000, 800), (5000, 1000)])
+# get x and y vectors
+x = points[:,0]
+y = points[:,1]
+
+# calculate polynomial
+z = np.polyfit(x, y, 3)
+f = np.poly1d(z)
+
+# calculate new x's and y's
+x_new = np.linspace(x[0], x[-1], 50)
+y_new = f(x_new)
+
+plt.plot(x,y,'o', x_new, y_new)
+plt.xlim([x[0]-1, x[-1] + 1 ])
+plt.show()
--- Part 2 - Regression/Section 6 - Polynomial Regression/regression_template_session.py
+++ Part 2 - Regression/Section 6 - Polynomial Regression/regression_template_session.py
@@ -6,13 +6,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Position_Salaries.csv')
-X = dataset.iloc[:, 1:2].values
+dataset = pd.read_csv('Cantilever_BeamThickness.csv')
+X = dataset.iloc[:, 1].values
 y = dataset.iloc[:, 2].values
 
 # Splitting the dataset into the Training set and Test set
-"""from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
